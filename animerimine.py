02b488c3a6507e7eda99af9473352ac398c5008b
opilane <opilane@HP-CZC2349HT9>
2023-04-19 12:25:47 +0300

    k

--- animerimine.py
+++ animerimine.py
@@ -1,4 +1,70 @@
 import pygame,sys
+import random
+def esimene():
+    global posx,posy
+    if posx == 0 and posy == 0:
+        sammx = 0
+        sammy = 1
+    if posx == 0 and posy >= Y - snoopy.get_rect().width:
+        sammx = 1
+        sammy = 0
+    if posx <= X - snoopy.get_rect().width and posy == Y - snoopy.get_rect().height:
+        sammx = 0
+        sammy = -1
+    if posx >= X - snoopy.get_rect().width and posy <= Y - snoopy.get_rect().width:
+        sammx = -1
+        sammy = 0
+    
+    
+    if posx==0 and posy>=Y-snoopy.get_rect().height:
+        sammx=0
+        sammy=1
+        sammy=-sammy 
+    posx+=sammx 
+    posy+=sammy
+    ekraan.blit(snoopy,(posx,posy))
+
+def teine():
+    global posx,posy
+    if posx>X-snoopy.get_rect().width or posx<0:
+        sammx=-sammx
+    if posy>X-snoopy.get_rect().height or posy<0:
+        sammy=-sammy 
+    if posy<0:
+        posy=0
+    elif posy>Y-snoopy.get_rect().height:
+        posy=Y-snoopy.get_rect().height
+
+    
+    if random.randint(0, 20) == 0:
+        sammy = random.choice([-1, -15])
+
+    posx+=sammx 
+    posy+=sammy
+    ekraan.blit(snoopy,(posx,posy)) 
+
+def kolmas(): 
+    global posx,posy 
+    if posx==0 and posy==0:
+        sammx=1
+        sammy=0
+    if posx==X-snoopy.get_rect().width and posy<=Y-snoopy.get_rect().height:
+        sammx=0
+        sammy=1
+    if posx<=X-snoopy.get_rect().width and posy==Y-snoopy.get_rect().height:
+        sammx=1
+        sammy=0
+        sammx=-sammx
+    
+    
+    if posx==0 and posy>=Y-snoopy.get_rect().height:
+        sammx=0
+        sammy=1
+        sammy=-sammy 
+    posx+=sammx 
+    posy+=sammy
+    ekraan.blit(snoopy,(posx,posy))
+
 pygame.init() 
 #värvid
 kollane=[255,255,10] 
@@ -17,7 +83,55 @@ snoopy=pygame.image.load("snoopy.png")
 posx=X-snoopy.get_rect().width
 posy=Y-snoopy.get_rect().height 
 lõpp=False 
-sammx=2 
+sammx = 2  
+sammy = 0
+while not lõpp:
+    kell.tick(60) 
+    events = pygame.event.get() 
+    for i in events: 
+        if i.type == pygame.QUIT: 
+            sys.exit() 
+        if random.randint(esimene,teine,kolmas):
+            break 
+"""sammx = -2  
+sammy = 0
+while not lõpp:
+    kell.tick(60) 
+    events = pygame.event.get() 
+    for i in events: 
+        if i.type == pygame.QUIT: 
+            sys.exit() 
+
+   if posx == 0 and posy == 0:
+        sammx = 0
+        sammy = 1
+    if posx == 0 and posy >= Y - snoopy.get_rect().width:
+        sammx = 1
+        sammy = 0
+    if posx <= X - snoopy.get_rect().width and posy == Y - snoopy.get_rect().height:
+        sammx = 0
+        sammy = -1
+    if posx >= X - snoopy.get_rect().width and posy <= Y - snoopy.get_rect().width:
+        sammx = -1
+        sammy = 0
+    
+    
+    if posx==0 and posy>=Y-snoopy.get_rect().height:
+        sammx=0
+        sammy=1
+        sammy=-sammy 
+    posx+=sammx 
+    posy+=sammy
+    ekraan.blit(snoopy,(posx,posy))
+   
+    pygame.display.flip() 
+    ekraan.fill(roheline)
+    pygame.quit"""
+
+
+
+
+"""sammx=2 
 sammy=0
 while not lõpp:
     kell.tick(60) 
@@ -27,17 +141,55 @@ while not lõpp:
             sys.exit() 
      
     if posx>X-snoopy.get_rect().width or posx<0:
-
         sammx=-sammx
-    if posy>Y-snoopy.get_rect().width or posy>5:
+    if posy>X-snoopy.get_rect().height or posy<0:
         sammy=-sammy 
-    if posx<0:
-        sammy=2 
-        sammx=0
-    posx-=sammx 
-    posy-=sammy
+    if posy<0:
+        posy=0
+    elif posy>Y-snoopy.get_rect().height:
+        posy=Y-snoopy.get_rect().height
+
+    
+    if random.randint(0, 20) == 0:
+        sammy = random.choice([-1, -15])
+
+    posx+=sammx 
+    posy+=sammy
     ekraan.blit(snoopy,(posx,posy))
-    #posx-=samm
+
     pygame.display.flip() 
     ekraan.fill(roheline)
-    pygame.quit
+    pygame.quit"""
+
+
+"""sammx=2 
+sammy=0
+while not lõpp:
+    kell.tick(60) 
+    events=pygame.event.get() 
+    for i in pygame.event.get(): 
+        if i.type==pygame.QUIT(): 
+            sys.exit() 
+    if posx==0 and posy==0:
+        sammx=1
+        sammy=0
+    if posx==X-snoopy.get_rect().width and posy<=Y-snoopy.get_rect().height:
+        sammx=0
+        sammy=1
+    if posx<=X-snoopy.get_rect().width and posy==Y-snoopy.get_rect().height:
+        sammx=1
+        sammy=0
+        sammx=-sammx
+    
+    
+    if posx==0 and posy>=Y-snoopy.get_rect().height:
+        sammx=0
+        sammy=1
+        sammy=-sammy 
+    posx+=sammx 
+    posy+=sammy
+    ekraan.blit(snoopy,(posx,posy))
+   
+    pygame.display.flip() 
+    ekraan.fill(roheline)
+    pygame.quit"""
